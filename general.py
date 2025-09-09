1683d9d01e96b7cc09af41385a50ee495e2c4f94
mdzgr <madalinazgreaban0@gmail.com>
2025-09-09 18:42:58 +0000

    add old function

--- general.py
+++ general.py
@@ -70,3 +70,32 @@ def load_data(json_input):
   else:
       raise ValueError("Input must be a filepath or a list of dictionaries.")
   return data
+
+  def combine_json_files(file_paths, output_file):
+    """
+    Combines multiple JSON files into a single json file.
+    """
+    combined_data = []
+    #for each file
+    for file_path in file_paths:
+        try:
+            with open(file_path, 'r') as f:
+                data = json.load(f) #open
+                if isinstance(data, list):
+                    combined_data.extend(data) #keeps flat structure
+                else:
+                    combined_data.append(data) #keeps nested structure
+            print(f"Successfully loaded and appended data from {file_path}")
+        except FileNotFoundError:
+            print(f"Error: File not found at {file_path}")
+        except json.JSONDecodeError:
+            print(f"Error: Could not decode JSON from {file_path}")
+        except Exception as e:
+            print(f"An unexpected error occurred while processing {file_path}: {e}")
+    try:
+        with open(output_file, 'w') as f:
+            json.dump(combined_data, f, indent=2)
+        print(f"Combined data successfully saved to {output_file}")
+    except Exception as e:
+        print(f"Error saving combined data to {output_file}: {e}")
+
